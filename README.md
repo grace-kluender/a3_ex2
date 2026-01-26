214f1eb1e67992bf6c8a03b9fb67995853e1c442
grace-kluender <graceek@uchicago.edu>
2026-01-26 16:49:10 -0600

    Adding files

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-Here is my README!
+This minimal Flask web API provides simple text utilities via URL path parameters.
+
+## Endpoints
+- `GET /`
+  - Returns a pretty-printed JSON list of available endpoints.
+
+- `GET /uppercase/<text>`
+  - Returns an uppercase version of `<text>`.
+
+- `GET /lowercase/<text>`
+  - Returns a lowercase version of `<text>`.
+
+- `GET /charcount/<text>`
+  - Returns the character count of `<text>`, excluding spaces.
+
+## Setup (Local)
+1. Clone this repo to your local machine
+2. Build the docker container:
+    docker build -t flask_app
+    docker run -p 5000:5000 flask_app
+    Open url that appears in terminal
+
---
+++ tests/test_app.py
@@ -0,0 +1,59 @@
+import pytest
+from app import app
+
+def test_uppercase():
+    """
+    Tests that a fully lowercase text input is returned fully capitalized.
+    """
+    app.testing = True 
+    client = app.test_client()
+    response = client.get("/uppercase/hello")
+    assert response.get_json() == "HELLO"
+
+def test_mixed_uppercase():
+    """
+    Tests that a partially lowercase text input is returned fully capitalized.
+    """
+    app.testing = True 
+    client = app.test_client()
+    response = client.get("/uppercase/Hello")
+    assert response.get_json() == "HELLO"
+
+def test_lowercase():
+    """
+    Tests that a fully uppercase text input is returned fully lowercase.
+    """
+    app.testing = True 
+    client = app.test_client()
+    response = client.get("/lowercase/HELLO")
+    assert response.get_json() == "hello"
+
+def test_mixed_lowercase():
+    """
+    Tests that a partially uppercase text input is returned fully lowercase.
+    """
+    app.testing = True 
+    client = app.test_client()
+    response = client.get("/lowercase/HeLLO")
+    assert response.get_json() == "hello"
+
+
+def test_charcount():
+    """
+    Tests that a standard string's character count is properly returned
+    """
+    app.testing = True 
+    client = app.test_client()
+    response = client.get("/charcount/hello")
+    assert response.get_json() == 5
+
+def test_charcount_ignores_spaces():
+    """
+    Tests that a string that includes spaces in it still returns the correct
+    character count (spaces are not characters)
+    """
+    app.testing = True
+    client = app.test_client()
+    response = client.get("/charcount/h%20ello")
+    assert response.get_json() == 5
+
