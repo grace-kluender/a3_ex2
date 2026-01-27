dc431871dd4a4af44161608450ad692066b83167
grace-kluender <graceek@uchicago.edu>
2026-01-27 12:54:03 -0600

    Black fixed formatting issues

--- app.py
+++ app.py
@@ -3,54 +3,53 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def home():
     data = {
-            "available_endpoints": {
-                "/uppercase/<text>":
-                    "Returns an uppercase version of the <text> "
-                    "from the URL path"
-                ,
-                "/lowercase/<text>":
-                    "Returns a lowercase version of the <text> "
-                    "from the URL path"
-                ,
-                "/charcount/<text>":
-                    "Returns the character count from the <text> "
-                    "from the URL path"
-            }
+        "available_endpoints": {
+            "/uppercase/<text>": "Returns an uppercase version of the <text> "
+            "from the URL path",
+            "/lowercase/<text>": "Returns a lowercase version of the <text> "
+            "from the URL path",
+            "/charcount/<text>": "Returns the character count from the <text> "
+            "from the URL path",
         }
+    }
     return Response(
         json.dumps(data, indent=2),
         mimetype="application/json",
     )
 
 
-@app.route("/uppercase/<text>", methods=['GET'])
+@app.route("/uppercase/<text>", methods=["GET"])
 def uppercase(text):
     """
     Returns an uppercase version of the text from the URL path
     """
     return jsonify(text.upper())
 
-@app.route("/lowercase/<text>", methods=['GET'])
+
+@app.route("/lowercase/<text>", methods=["GET"])
 def lowercase(text):
     """
     Returns a lowercase version of the text from the URL path
     """
     return jsonify(text.lower())
 
-@app.route("/charcount/<text>", methods=['GET'])
+
+@app.route("/charcount/<text>", methods=["GET"])
 def charactercount(text):
     """
     Returns the character count from the text in the URL path, excluding spaces
     """
     count = 0
     for char in text:
-        if char != ' ':
+        if char != " ":
             count += 1
 
     return jsonify(count)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,38 +1,42 @@
 import pytest
 from app import app
 
+
 def test_uppercase():
     """
     Tests that a fully lowercase text input is returned fully capitalized.
     """
-    app.testing = True 
+    app.testing = True
     client = app.test_client()
     response = client.get("/uppercase/hello")
     assert response.get_json() == "HELLO"
+
 
 def test_mixed_uppercase():
     """
     Tests that a partially lowercase text input is returned fully capitalized.
     """
-    app.testing = True 
+    app.testing = True
     client = app.test_client()
     response = client.get("/uppercase/Hello")
     assert response.get_json() == "HELLO"
+
 
 def test_lowercase():
     """
     Tests that a fully uppercase text input is returned fully lowercase.
     """
-    app.testing = True 
+    app.testing = True
     client = app.test_client()
     response = client.get("/lowercase/HELLO")
     assert response.get_json() == "hello"
+
 
 def test_mixed_lowercase():
     """
     Tests that a partially uppercase text input is returned fully lowercase.
     """
-    app.testing = True 
+    app.testing = True
     client = app.test_client()
     response = client.get("/lowercase/HeLLO")
     assert response.get_json() == "hello"
@@ -42,10 +46,11 @@ def test_charcount():
     """
     Tests that a standard string's character count is properly returned
     """
-    app.testing = True 
+    app.testing = True
     client = app.test_client()
     response = client.get("/charcount/hello")
     assert response.get_json() == 5
+
 
 def test_charcount_ignores_spaces():
     """
@@ -56,4 +61,3 @@ def test_charcount_ignores_spaces():
     client = app.test_client()
     response = client.get("/charcount/h%20ello")
     assert response.get_json() == 5
-
